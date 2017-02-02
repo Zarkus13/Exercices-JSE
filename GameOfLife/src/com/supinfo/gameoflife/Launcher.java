96352c7d5e7baae835d905ddf367beda61c636cd
Alexis Weil <zarkus13@gmail.com>
2017-02-02 14:59:24 +0100

    Asking for user the number of columns and rows for the game of life

--- GameOfLife/src/com/supinfo/gameoflife/Launcher.java
+++ GameOfLife/src/com/supinfo/gameoflife/Launcher.java
@@ -1,6 +1,8 @@
 package com.supinfo.gameoflife;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 
 public class Launcher {
 
@@ -8,10 +10,41 @@ public class Launcher {
      * @param args
      */
     public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        int nbCols = 0, nbRows = 0;
+        int minCols = 10, minRows = 10;
+        
+        System.out.println("Please, enter a number of columns :");
+        for(boolean cont = true ; cont ;) {
+            try {
+                nbCols = new Scanner(System.in).nextInt();
+                
+                if(nbCols < minCols)
+                    throw new InputMismatchException();
+                
+                cont = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Please, enter a valid number of columns (min " + minCols + ")");
+            }
+        }
+        
+        System.out.println("Please, enter a number of rows :");
+        for(boolean cont = true ; cont ;) {
+            try {
+                nbRows = new Scanner(System.in).nextInt();
+                
+                if(nbRows < minRows)
+                    throw new InputMismatchException();
+                
+                cont = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Please, enter a valid number of rows (min " + minRows + ")");
+            }
+        }
 
-        World world = new World(10, 10);
+        World world = new World(nbCols, nbRows);
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 3; i++) {
             world.newGeneration();
 
             System.out.println("World n°" + (i + 1) + " : \n");
